6b8c46738aeca02e645636146e00a0082444f407
Eileen-Qian <yiqianc.14@gmail.com>
2026-02-13 15:11:11 -0800

    feat: Day45
    dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import { useEffect } from 'react';
 import './App.css';
-import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+import { HashRouter, NavLink, Routes, Route, useNavigate, Outlet, useParams, useOutletContext } from 'react-router-dom';
 
 const Logout = () => {
   const navigate = useNavigate();
@@ -9,6 +9,10 @@ const Logout = () => {
       <button onClick={() => navigate("/login")}>登出</button>
     </>
   )
+}
+
+const Home = () => {
+  return <p>這是首頁</p>
 }
 
 const Todo = () => {
@@ -25,6 +29,64 @@ const Login = () => {
 const Register = () => {
   return <p>這是註冊頁面</p>;
 };
+
+const Post = () => {
+
+  const data = [
+    {
+      id: 1,
+      title: "Post1",
+      content: "這是post1",
+    },
+    {
+      id: 2,
+      title: "Post2",
+      content: "這是post2",
+    },
+    {
+      id: 3,
+      title: "Post3",
+      content: "這是post3",
+    },
+  ];
+
+  return (
+    <>
+      <p>這是 Post 頁面</p>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <NavLink to={`/post/${item.id}`}>{item.title}</NavLink>
+          </li>
+        ))}
+      </ul>
+      <Outlet context={data}/>
+    </>
+  )
+}
+const SinglePost = () => {
+  const navigate = useNavigate()
+  let params = useParams();
+  const { postId } = params;
+  const data = useOutletContext();
+  const singleData = data.find((e) => e.id === Number(postId));
+  useEffect(() => {
+    if (!singleData) {
+      navigate("*");
+    }
+  }, [singleData, navigate]);
+
+  if (!singleData) {
+    return null;
+  }
+  return (
+    <>
+      <p>這是 post 細節頁面</p>
+      <p>Post id: {postId}</p>
+      {singleData.content}
+    </>
+  )
+}
 const NotFound = () => {
   return <p>這是 404 頁面</p>
 }
@@ -46,12 +108,19 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='register' element={<Register />} />
           <Route path='login' element={<Login />} />
           <Route path='todo' element={<Todo />} />
+          <Route path='post' element={<Post />}>
+            <Route path=':postId' element={<SinglePost />} />
+          </Route>
           <Route path='*' element={<NotFound />} />
         </Routes>
         {/* 練習區 */}
